c07853373bd00e2df2b275f08ad127a59d80ec49
shinjong93 <shinjong9312@gmail.com>
2018-05-25 01:32:14 +0900

    Update afterProcess.py

--- afterProcess.py
+++ afterProcess.py
@@ -7,26 +7,27 @@ ROOT = "../result/"
 def process(f):
         n1 = 0
         lines = []
-        while True:
+        with open(f, 'r+') as f:
+            while True:
                 line = f.readline()
                 if not line: break
-                for i in range(len(line)-1):
+                for i in range(len(line) - 1):
                     if line[i] == "{":
                         n1 += 1
                     if line[i] == ':':
-                        line = line[:i] + '=' + line[i+1:]
+                        line = line[:i] + '=' + line[i + 1:]
                 lines.append(line)
-        f.seek(0)
-        f.writelines(lines)
-        f.close()
+            f.seek(0)
+            f.writelines(lines)
+            f.close()
 
+        with open(f, 'a') as f:
+            while (n1 > 0):
+                f.write("\n}")
+                n1 -= 1
 
-        while(n1 > 0):
-            f.write("\n}")
-            n1 -= 1
-
-
-        while True:
-            line = f.readline()
-            if not line: break
-            print(line, end='')
+        with open(f, 'r+') as f:
+            while True:
+                line = f.readline()
+                if not line: break
+                print(line, end='')
